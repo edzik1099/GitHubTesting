65994be62ddddcb690f2a192799cecc4280278cb
Evan Dzik <edzik1099@stu.d214.org>
2020-10-20 13:19:31 -0700

    Initial Commit

---
+++ GitHubTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTesting
+//
+//  Created by user171414 on 10/20/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
